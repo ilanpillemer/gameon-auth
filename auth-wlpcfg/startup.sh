9673fdc54932fe886371f2b5eb05a00df6faccc4
Erin Schnabel <ebullient@users.noreply.github.com>
2016-09-14 23:08:29 -0400

    Revise Dockerfile, cleanup overrides, remove logstash
    Signed-off-by: Erin Schnabel <ebullient@users.noreply.github.com>

--- auth-wlpcfg/startup.sh
+++ auth-wlpcfg/startup.sh
@@ -5,9 +5,23 @@ export A8_SERVICE=auth:v1
 export A8_ENDPOINT_PORT=9443
 export A8_ENDPOINT_TYPE=https
 
+if [ "$SERVERDIRNAME" == "" ]; then
+  SERVERDIRNAME=defaultServer
+else
+  # Share the configuration directory via symlink
+  ln -s /opt/ibm/wlp/usr/servers/defaultServer /opt/ibm/wlp/usr/servers/$SERVERDIRNAME
+
+  # move the convenience output dir link to the new output location
+  rm /output
+  ln -s $WLP_OUTPUT_DIR/$SERVERDIRNAME /output
+fi
+
+SERVER_PATH=/opt/ibm/wlp/usr/servers/$SERVERDIRNAME
+mkdir -p ${SERVER_PATH}/configDropins/overrides
+
 if [ "$SSL_CERT" != "" ]; then
   echo Found an SSL cert to use.
-  cd /opt/ibm/wlp/usr/servers/defaultServer/resources/
+  cd ${SERVER_PATH}/resources/
   echo -e $SSL_CERT > cert.pem
   openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in cert.pem
   keytool -import -v -trustcacerts -alias default -file cert.pem -storepass truststore -keypass keystore -noprompt -keystore security/truststore.jks
@@ -32,7 +46,7 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   done
   echo "etcdctl returned sucessfully, continuing"
 
-  cd /opt/ibm/wlp/usr/servers/defaultServer/resources/
+  cd ${SERVER_PATH}/resources/
   etcdctl get /proxy/third-party-ssl-cert > cert.pem
   openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in cert.pem
   keytool -import -v -trustcacerts -alias default -file cert.pem -storepass truststore -keypass keystore -noprompt -keystore security/truststore.jks
@@ -63,9 +77,7 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
 
   #to run with message hub, we need a jaas jar we can only obtain
   #from github, and have to use an extra config snippet to enable it.
-  cd /opt/ibm/wlp/usr/servers/defaultServer
-  mkdir -p configDropins/overrides
-  mv kafkaDropin.xml configDropins/overrides
+  cp ${SERVER_PATH}/configDropins/messageHub.xml ${SERVER_PATH}/configDropins/overrides
   wget https://github.com/ibm-messaging/message-hub-samples/raw/master/java/message-hub-liberty-sample/lib-message-hub/messagehub.login-1.0.0.jar
 
   # Softlayer needs a logstash endpoint so we set up the server
@@ -88,5 +100,8 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
 else
   echo A8_ENDPOINT_TYPE=${A8_ENDPOINT_TYPE}
   echo A8_ENDPOINT_PORT=${A8_ENDPOINT_PORT}
+
+  cp ${SERVER_PATH}/configDropins/localDev.xml ${SERVER_PATH}/configDropins/overrides
+
   exec a8sidecar --log --proxy --register --supervise /opt/ibm/wlp/bin/server run defaultServer
 fi
